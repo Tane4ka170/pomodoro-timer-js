61c41db5360d623e7c75a6de32166654c3c93e8b
Tane4ka170 <tane4ka170@gmail.com>
2025-08-16 23:59:10 +0300

    Feat: Added timer functions and sound effects

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
 var minutes = 25;
 var seconds = 0;
 
+var click = new Audio("click.mp3");
+var bell = new Audio("bell.mp3");
+
 function template() {
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
+}
+
+function start() {
+  click.play();
+
+  minutes = 24;
+  seconds = 59;
+
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  var minutes_interval = setInterval(minutesTimer, 60000);
+  var seconds_interval = setInterval(secondsTimer, 1000);
+
+  function minutesTimer() {
+    minutes = minutes - 1;
+    document.getElementById("minutes").innerHTML = minutes;
+  }
+}
+function secondsTimer() {
+  seconds = seconds - 1;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (seconds <= 57) {
+    if (minutes <= 24) {
+      clearInterval(minutes_interval);
+      clearInterval(seconds_interval);
+      document.getElementById("done").innerHTML("Session done!Take a break");
+      document.getElementById("done").classList.add("show_message");
+      bell.play();
+    }
+    seconds = 60;
+  }
 }
